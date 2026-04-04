e5bcb5316820120f85aff17768f2c8c42ef35f93
adelinajardan <adelina.jardan@student.upt.ro>
2026-04-04 21:27:22 +0300

    Varianta 4: Am adaugat stalpi care se misca

--- main.c
+++ main.c
@@ -5,18 +5,47 @@ int main(void) {
     const int ecranLatime = 450;
     const int ecranInaltime = 700;
 
-    InitWindow(ecranLatime, ecranInaltime, "Flappy Bird - Pasul 3: Gravitatie");
+    InitWindow(ecranLatime, ecranInaltime, "Flappy Bird - Pasul 4: Obstacole");
 
     // Datele pasarii
     Vector2 pozitiePasare = { 100, ecranInaltime / 2 };
     float vitezaPasare = 0;
-    float gravitatie = 0.5f;
+    float gravitatie = 0.5f; //cat de repede sau incet cade(mai mare mai greu)
     float fortaSarituta = -8.0f;
+
+    //Stalpi
+    Rectangle stalpSus={450,0,50,250};
+    Rectangle stalpJos={4500,450,50,250};
+    float vitezaStalp=3.0f;
 
     SetTargetFPS(60);
 
     while (!WindowShouldClose()) {
         // 1. LOGICA (Update)
+        vitezaPasare +=gravitatie;
+        pozitiePasare.y+= vitezaPasare;
+
+        if (IsKeyPressed(KEY_SPACE)) vitezaPasare= -7.0f;
+
+        //miscam stalpii spre stanga
+        stalpSus.x -=vitezaStalp;
+        stalpJos.x -=vitezaStalp;
+
+        //daca stalp a iesit din ecran il ppunem iara in dreapta
+        if (stalpSus.x <-50)
+        {
+            stalpSus.x=ecranLatime;
+            stalpJos.x=ecranLatime;
+        }
+       //Desenare stalp
+       BeginDrawing();
+            ClearBackground(SKYBLUE);
+            //Desenam stalpi
+            DrawRectangleRec(stalpSus, GREEN);
+            DrawRectangleRec(stalpJos, GREEN);
+
+            // Desenam pasarea (un cerc galben momentan)
+            DrawCircleV(pozitiePasare, 20, YELLOW);
         
         // Pasarea cade constant din cauza gravitatiei
         vitezaPasare += gravitatie;
@@ -27,12 +56,7 @@ int main(void) {
             vitezaPasare = fortaSarituta;
         }
 
-        // 2. DESENARE (Draw)
-        BeginDrawing();
-            ClearBackground(SKYBLUE);
-
-            // Desenam pasarea (un cerc galben momentan)
-            DrawCircleV(pozitiePasare, 20, YELLOW);
+        
 
             DrawText("Apasa SPACE ca sa sari!", 10, 10, 20, DARKGRAY);
         EndDrawing();
@@ -40,4 +64,5 @@ int main(void) {
 
     CloseWindow();
     return 0;
+
 }
